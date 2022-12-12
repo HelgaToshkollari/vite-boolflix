6878635a44cdce8b7f6d03a6fe4f838ee3f44d50
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-12-12 17:14:09 +0100

    adds components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
+<template>
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
+</template>
+
 <script>
+import AppHeader from "./components/AppHeader.vue"
+import AppMain from "./components/AppMain.vue"
+import axios from "axios";
+import { store } from "./store";
+export default{
+  components:{AppMain , AppHeader},
+  data(){
+    return{
+      store
+    }
+  },
+  fetchMovie (){
+    axios.get( store.baseApiUrl,{
+        
+      params:{
+          api_key: "1c6cb7d9e9e6fbde55731500b8758dd6",
+          query:"",
+      }
+      
+    })
+  }
+}
 
 </script>
 
-<template>
-
-</template>
-
-<style scoped>
+<style lang="scss">
+@use "./styles/general.scss";
 
 </style>
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import {reactive} from "vue";
+
+
+export const store = reactive ({
+    baseApiUrl: "https://api.themoviedb.org/3"
+
+});
+
