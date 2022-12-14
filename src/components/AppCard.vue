768b9ac344e3cd29ea4c66c6cf8957ddc1a4f113
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-12-14 11:08:36 +0100

    adds fixed stars

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -3,11 +3,22 @@
         <div class="card" style="width: 100%;">
             <img class="card-img-top" :src="`https://image.tmdb.org/t/p/w342/${mediaInfos.poster_path}`" >
             <div class="card-body">
-                <h5 class="card-title">{{mediaInfos.title}}</h5>
+                <h5 class="card-title">{{mediaInfos.title}}{{mediaInfos.name}}</h5>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item">{{mediaInfos.original_title}}{{mediaInfos.name}}</li>
+                    <li class="list-group-item">{{mediaInfos.original_title}}{{mediaInfos.original_name}}</li>
                     <li class="list-group-item "><span v-if="flagsForLang()" :class="flagsForLang()"></span> <span v-else class="fi fi-xx">{{mediaInfos.original_language}}  </span></li>
-                    <li class="list-group-item">{{vote()}} <i v-for="star in vote()" class="fa-solid fa-star text-warning"></i></li>
+                    <li class="list-group-item ">   
+                        <div class="my-stars"> 
+                            <p>Vote :</p>
+                            <div class="empty-star">
+                                <i v-for="star in 5" class="fa-regular fa-star text-warning "></i> 
+                            </div>
+                            <div class="full-star">
+                                <i v-for="star in vote()" class="fa-solid fa-star text-warning "></i>
+                            </div>
+                        </div>
+                        
+                    </li>
                 </ul>
             </div>
             
@@ -54,7 +65,7 @@ export default {
 
        },
        vote(){
-        return Math.ceil(this.mediaInfos.vote_average);
+        return Math.ceil(this.mediaInfos.vote_average / 2);
        }
        
     }
@@ -63,5 +74,18 @@ export default {
 }
 </script>
 <style lang="scss">
+@use '../styles/partials/variables' as *;
+
+.my-stars{
+    position: relative;
+    .empty-star , .full-star{
+        position: absolute;
+        bottom: 0;
+        left: 3.1rem;
+        
+    }
+    
+
+}
   
 </style>
